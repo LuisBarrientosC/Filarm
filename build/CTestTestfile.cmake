d39ee7d66d263d02209f8c125919f2057292a44a
LuisBarrientosC <136543601+LuisBarrientosC@users.noreply.github.com>
2023-09-13 23:51:38 -0400

    Add files via upload

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,13 @@
+# CMake generated Testfile for 
+# Source directory: /home/yelidi/ar3_ws/src
+# Build directory: /home/yelidi/ar3_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("ar3_gazebo")
+subdirs("ar3_hardware_drivers")
+subdirs("ar3_description")
+subdirs("ar3_hardware_interface")
+subdirs("ar3_moveit_config")
+subdirs("ar3_control")
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/yelidi/ar3_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/yelidi/ar3_ws/devel;/home/yelidi/cvbridge_build_ws/install;/opt/ros/melodic'.split(';') if '/home/yelidi/ar3_ws/devel;/home/yelidi/cvbridge_build_ws/install;/opt/ros/melodic' != '' else []
