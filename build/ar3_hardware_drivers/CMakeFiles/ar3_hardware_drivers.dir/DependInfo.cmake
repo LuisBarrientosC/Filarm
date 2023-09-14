0781c2c4cf394cb63887cbb04550f625247adade
LuisBarrientosC <136543601+LuisBarrientosC@users.noreply.github.com>
2023-09-14 00:23:12 -0400

    Add files via upload

---
+++ build/ar3_hardware_drivers/CMakeFiles/ar3_hardware_drivers.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/yelidi/ar3_ws/src/ar3_hardware_drivers/src/TeensyDriver.cpp" "/home/yelidi/ar3_ws/build/ar3_hardware_drivers/CMakeFiles/ar3_hardware_drivers.dir/src/TeensyDriver.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ROSCONSOLE_BACKEND_LOG4CXX"
+  "ROS_BUILD_SHARED_LIBS=1"
+  "ROS_PACKAGE_NAME=\"ar3_hardware_drivers\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/yelidi/ar3_ws/src/ar3_hardware_drivers/include"
+  "/home/yelidi/ar3_ws/src/ar3_hardware_drivers/include/ar3_hardware_drivers"
+  "/opt/ros/melodic/include"
+  "/opt/ros/melodic/share/xmlrpcpp/cmake/../../../include/xmlrpcpp"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/ar3_hardware_drivers/CMakeFiles/ar3_hardware_drivers.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ar3_hardware_drivers.dir/src/TeensyDriver.cpp.o"
+  "/home/yelidi/ar3_ws/devel/lib/libar3_hardware_drivers.pdb"
+  "/home/yelidi/ar3_ws/devel/lib/libar3_hardware_drivers.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ar3_hardware_drivers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
