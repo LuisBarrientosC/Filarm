425e129a573c2ddd9e53f5ec179bd4e48fdec87e
LuisBarrientosC <136543601+LuisBarrientosC@users.noreply.github.com>
2023-09-14 00:19:59 -0400

    Add files via upload

---
+++ build/ar3_hardware_drivers/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/yelidi/ar3_ws/src/ar3_hardware_drivers/include;/usr/include".split(';') if "/home/yelidi/ar3_ws/src/ar3_hardware_drivers/include;/usr/include" != "" else []
+PROJECT_CATKIN_DEPENDS = "roscpp;rospy;std_msgs".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lar3_hardware_drivers;/usr/lib/x86_64-linux-gnu/libboost_system.so".split(';') if "-lar3_hardware_drivers;/usr/lib/x86_64-linux-gnu/libboost_system.so" != "" else []
+PROJECT_NAME = "ar3_hardware_drivers"
+PROJECT_SPACE_DIR = "/home/yelidi/ar3_ws/devel"
+PROJECT_VERSION = "0.0.0"
